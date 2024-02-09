c757e10a29d1de5c502b55002d49bb0fa22dc720
Tulipreactjsmain <rdhnft@gmail.com>
2024-02-09 12:48:01 +0100

    modified:   script.js

--- script.js
+++ script.js
@@ -14,29 +14,35 @@ function computerPlay() {
 const computerChoice = computerPlay();
 
 function playRound(playerSelection, computerSelection) {
-    const winningConditions = {
-      rock: "scissors",
-      paper: "rock",
-      scissors: "paper",
-    };
-    
-    if (!winningConditions.hasOwnProperty(playerSelection.toLowerCase())) {
-      return "Invalid selection. Please choose rock, paper, or scissors.";
-    }
-    
-    playerSelection = playerSelection.toLowerCase();
-    computerSelection = computerSelection.toLowerCase();
-  
-    if (playerSelection === computerSelection) {
-      return "It's a tie!";
-    } else if (winningConditions[playerSelection] === computerSelection) {
-      return `You Win! ${playerSelection} beats ${computerSelection}`;
-    } else {
-      return `You Lose! ${computerSelection} beats ${playerSelection}`;
-    }
+  const winningConditions = {
+    rock: "scissors",
+    paper: "rock",
+    scissors: "paper",
+  };
+
+  if (!winningConditions.hasOwnProperty(playerSelection.toLowerCase())) {
+    return "Invalid selection. Please choose rock, paper, or scissors.";
   }
-  
-  const playerSelection = "rock";
-  
-  console.log(playRound(playerSelection, computerChoice));
-  
+
+  playerSelection = playerSelection.toLowerCase();
+  computerSelection = computerSelection.toLowerCase();
+
+  if (playerSelection === computerSelection) {
+    return "It's a tie!";
+  } else if (winningConditions[playerSelection] === computerSelection) {
+    return `You Win! ${playerSelection} beats ${computerSelection}`;
+  } else {
+    return `You Lose! ${computerSelection} beats ${playerSelection}`;
+  }
+}
+
+const playerSelection = "rock";
+
+function game() {
+  for (let i = 0; i < 5; i++) {
+    console.log(playRound(playerSelection, computerPlay()));
+  }
+}
+
+game();
+
